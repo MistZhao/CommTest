9054a88d98a03abd1932701694efdfb3f55d0d53
MistZhao <526800129@qq.com>
2017-03-02 14:56:54 +0800

    first commit

---
+++ Channel/Channel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterfaceDll;
+
+namespace Channel
+{
+    public class Channel
+    {
+        #region 公有函数
+        public Channel(string strChannelPath)
+        {
+            m_strChannelPath = strChannelPath;
+            m_nPortNum = 0;
+            m_objCommTypes.Clear();
+        }
+
+        public void SetCommTypes()
+        {
+            
+        }
+
+        public void SetProtocal(IProtocal objProtocal)
+        {
+            m_objProtocal = objProtocal;
+        }
+
+        public List<ICommType> GetCommTypes()
+        {
+            return m_objCommTypes;
+        }
+        #endregion
+
+        #region 私有函数
+        #endregion
+
+        #region 变量
+        private string m_strChannelPath;
+        private int m_nPortNum;
+        private List<ICommType> m_objCommTypes;
+        private IProtocal m_objProtocal;
+        #endregion
+    }
+}
---
+++ Channel/ChannelFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InterfaceDll;
+
+namespace Channel
+{
+    public class ChannelFactory
+    {
+        #region 公有函数
+        public static Channel CreateChannel(string strChannelName)
+        {
+            Channel objChannel = new Channel(strChannelName);
+            objChannel.SetCommTypes();
+            return objChannel;
+        }
+        #endregion
+
+        #region 私有函数
+
+        #endregion
+
+        #region 变量
+        #endregion
+    }
+}
