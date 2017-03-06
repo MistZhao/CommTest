8d7f0cd1ce7bd67cb6a3db0c9124ae8674f9b107
xingxing <xingxing@DESKTOP-DOQ0R5Q>
2017-03-06 22:44:13 +0800

    Signed-off-by: xingxing <xingxing@DESKTOP-DOQ0R5Q>

--- InterfaceDll/AbstractDataSource.cs
+++ InterfaceDll/AbstractDataSource.cs
@@ -8,6 +8,20 @@ namespace InterfaceDll
     abstract public class AbstractDataSource:IProtocal
     {
         private Queue<string> objMsgQueue = new Queue<string>();
+        private System.Timers.Timer objTimer = new System.Timers.Timer();
+
+        public AbstractDataSource()
+        {
+            objTimer.Interval = 500;
+            objTimer.Elapsed += ObjTimer_Elapsed;
+            objTimer.Start();
+        }
+
+        private void ObjTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            string strMsg = "产生遥测遥信";
+            ReceiveMsg(strMsg);
+        }
 
         public virtual Queue<string> SendMsg()
         {
@@ -15,7 +29,7 @@ namespace InterfaceDll
         }
         public virtual void ReceiveMsg(string strMsg)
         {
-
+            objMsgQueue.Enqueue(strMsg);
         }
     }
 }
